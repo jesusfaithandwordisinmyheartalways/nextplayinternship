9cce4c5c8a476083d536aca75cee28a46649df67
Andrew Johnson <90214404+jesusfaithandwordisinmyheartalways@users.noreply.github.com>
2025-05-29 16:23:04 -0500

    Update socket.ts

--- client/src/utils/socket.ts
+++ client/src/utils/socket.ts
@@ -4,6 +4,6 @@
 
 import { io } from 'socket.io-client'
 
-export const socket = io('http://localhost:3001', {
+export const socket = io('https://nextplayinternshipserver.onrender.com', {
     transports: ['websocket'],
   });
